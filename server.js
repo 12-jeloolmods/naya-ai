dc8f409003245e7051d23066f497c25390336cf9
JeloolMods <yantocloudreal@gmail.com>
2026-04-08 00:42:42 +0700

    Change server port from 3000 to 8080

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ app.post("/chat", async (req, res) => {
 });
 
 // ⚠️ WAJIB pakai ini di Railway
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log("Server jalan di port " + PORT);
+  console.log("Server jalan di port" + PORT);
 });
